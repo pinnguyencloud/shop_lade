f91160db5a1b8bdbb7d3d2bfe80169f718338a29
Huy Nguyễn <huypkayer@gmail.com>
2024-12-03 11:12:22 +0700

    supplier

--- FE/managementWebApp/src/services/Accounts/suppliers/suppliersServices.js
+++ FE/managementWebApp/src/services/Accounts/suppliers/suppliersServices.js
@@ -17,3 +17,30 @@ export const addNewSuppliers = async (payload) => {
     console.error("Đã có lỗi xảy ra ở addNewSuppliers:", error.message);
   }
 };
+
+export const getSuppliersID = async (id) => {
+  try {
+    const response = await apiSuppliers.get(id);
+    return response.data;
+  } catch (error) {
+    console.error("Đã có lỗi xảy ra ở getSuppliersID:", error.message);
+  }
+};
+
+export const updateSuppliersID = async (id, payload) => {
+  try {
+    const response = await apiSuppliers.put(id, JSON.stringify(payload));
+    return response.data;
+  } catch (error) {
+    console.error("Đã có lỗi xảy ra ở updateSuppliersID:", error.message);
+  }
+};
+
+export const deteleSuppliersID = async (id) => {
+  try {
+    const response = await apiSuppliers.delete(id);
+    return response.data;
+  } catch (error) {
+    console.error("Đã có lỗi xảy ra ở deteleSuppliersID:", error.message);
+  }
+};
